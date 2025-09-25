084b951bb328a8ea88e3d047b927ab9d03561046
Fatima Rakiep <rakiepfatima@gmail.com>
2025-09-25 21:59:38 +0200

    Create overlay element and toggle sidebar visibility and update icon and text

--- scripts/ui/sidebar.js
+++ scripts/ui/sidebar.js
@@ -4,3 +4,26 @@ export function setupSidebarToggle() {
   const toggleIcon = document.getElementById("sidebar-toggle-icon");
   const toggleText = document.getElementById("sidebar-toggle-text");
   const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+
+  let overlay = document.querySelector(".sidebar-overlay");
+  if (!overlay) {
+    overlay = document.createElement("div");
+    overlay.className = "sidebar-overlay";
+    document.body.appendChild(overlay);
+  }
+
+  toggleBtn?.addEventListener("click", () => {
+    const isHidden = sidebar?.classList.toggle("hidden");
+
+    if (isHidden) {
+      toggleIcon.src = "./assets/icon-show-sidebar.svg";
+      toggleIcon.alt = "show-sidebar-icon";
+      toggleText.textContent = "Show Sidebar";
+      toggleBtn.classList.add("show-mode");
+    } else {
+      toggleIcon.src = "./assets/icon-hide-sidebar.svg";
+      toggleIcon.alt = "hide-sidebar-icon";
+      toggleText.textContent = "Hide Sidebar";
+      toggleBtn.classList.remove("show-mode");
+    }
+  });
